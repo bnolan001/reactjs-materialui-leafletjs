dbbd8f9f477c870cf53c98b0fa4ef97945100d0a
bnolan001 <bnolan001@gmail.com>
2022-01-07 20:10:18 -0500

    Added code for leaflet map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './style.css';
 import { SampleCard } from './sampleCard';
+import { SampleMap } from './sampleMap';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <SampleCard></SampleCard>
+      <SampleCard />
+      <SampleMap />
     </div>
   );
 }
