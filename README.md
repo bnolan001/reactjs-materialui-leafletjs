6b9a5b9e30092cb0fd1fb2d6cde03ea070033637
bnolan001 <bnolan001@gmail.com>
2022-01-07 19:24:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-nw4cbz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-nw4cbz)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+import { SampleCard } from './sampleCard';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <SampleCard></SampleCard>
+    </div>
+  );
+}
