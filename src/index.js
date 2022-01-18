cec8e0084f5f1d9300669a351c4442fd19955d24
bnolan001 <bnolan001@gmail.com>
2022-01-17 22:23:36 -0500

    1 file modified

--- src/index.js
+++ src/index.js
@@ -5,7 +5,18 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import { HashRouter } from "react-router-dom";
 
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href");
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+      <HashRouter>
+        <App />
+      </HashRouter>,
+  rootElement
+);
